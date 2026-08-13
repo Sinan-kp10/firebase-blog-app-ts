3875c66b7007c82f1149169ac212c5b921d4a38b
Sinan-kp10 <sinankp9503@gmail.com>
2026-08-13 20:54:21 +0530

    fix : Moved all types inside Types folder

--- src/types/auth.ts
+++ src/types/auth.ts
@@ -1,6 +1,13 @@
 import { z } from "zod";
 import { signupSchema } from "../validation/signupSchema";
 import { loginSchema } from "../validation/loginSchema";
+import type { ReactNode } from "react";
 
 export type SignupForm = z.infer<typeof signupSchema>;
 export type LoginForm = z.infer<typeof loginSchema>;
+
+
+
+export type UserProviderProps = {
+  children: ReactNode;
+};
